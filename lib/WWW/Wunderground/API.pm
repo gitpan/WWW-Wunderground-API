c7386c732250a5f557e0abb0a0fa40debaae1017
John Lifsey <nebulous@crashed.net>
2011-10-17 15:53:35 +0000

    Import of NEBULOUS/WWW-Wunderground-API-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-Wunderground-API
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         NEBULOUS/WWW-Wunderground-API-0.02.tar.gz
    gitpan-cpan-author:       NEBULOUS
    gitpan-cpan-maturity:     released

--- lib/WWW/Wunderground/API.pm
+++ lib/WWW/Wunderground/API.pm
@@ -4,33 +4,44 @@ use 5.006;
 use Any::Moose;
 use LWP::Simple;
 use XML::Simple;
+use JSON::Any;
 use Hash::AsObject;
 
 =head1 NAME
 
-WWW::Wunderground::API - Use Weather Underground's XML interface
+WWW::Wunderground::API - Use Weather Underground's XML or JSON interface
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
-
+has api_key => (is=>'ro', isa=>'Str');
+has api_type => (is=>'rw', default=>sub {$_[0]->api_key ? 'json' : 'xml'});
 has location => (is=>'rw', required=>1, trigger=>\&update);
 has xml => (is=>'rw', isa=>'Str');
+has json => (is=>'rw', isa=>'Str');
 has data => (is=>'rw',isa=>'Hash::AsObject');
 
 
 
 sub update {
   my $self = shift;
-  my $xml = get('http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query='.$self->location);
-  if ($xml) {
-    $self->xml($xml);
-    $self->data(Hash::AsObject->new(XMLin($xml)));
+  if ($self->api_type eq 'json') {
+    my $json = get('http://api.wunderground.com/api/'.$self->api_key.'/conditions/q/'.$self->location.'.json');
+    if ($json) {
+      $self->json($json);
+      $self->data(Hash::AsObject->new(JSON::Any->jsonToObj($json)->{current_observation})); 
+    }
+  } else {
+    my $xml = get('http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query='.$self->location);
+    if ($xml) {
+      $self->xml($xml);
+      $self->data(Hash::AsObject->new(XMLin($xml)));
+    }
   }
 }
 
@@ -43,6 +54,19 @@ around BUILDARGS => sub {
     return $class->$orig(@_);
   }
 };
+
+sub AUTOLOAD {
+  my $self = shift;
+  our $AUTOLOAD;
+  my ($key) = $AUTOLOAD =~ /::(\w+)$/;
+  my $val = $self->data->$key;
+  if (defined($val)) {
+    return $val;
+  } else {
+    warn "$key is not defined. Is it a valid key, and is data actually loading?";
+    return undef;
+  }
+}
 
 no Any::Moose;
 
@@ -60,13 +84,19 @@ to see all of the tasty data bits.
     my $wun = new WWW::Wunderground::API('Fairfax, VA');
 
     #or zipcode
-    my $wun = new WWW::Wunderground::API(22030);
+    my $wun = new WWW::Wunderground::API('22030');
 
     #or airport identifier
     my $wun = new WWW::Wunderground::API('KIAD');
 
-    print 'The temperature is: '.$wun->data->temp_f;
-    print 'XML source:'.$wun->xml;
+    #using the json API.
+    my $wun = new WWW::Wunderground::API(location=>'KIAD', api_key=>'your wunderground API key');
+
+    print 'The temperature is: '.$wun->data->temp_f."\n";
+    print 'The rest of the world calls that: '.$wun->temp_c."\n"; #Keys are AUTOLOADed to $wun->data->$key for lazy typers.
+    print 'XML source:'.$wun->xml if $wun->api_type eq 'xml';
+    print 'JSON source:'.$wun->json if $wun->api_type eq 'json';
+
 
 =head2 update()
 
@@ -76,20 +106,34 @@ Refetch data from the server. This is called automatically every time location i
 
 Change the location. For example:
 
-    my $wun = new WWW::Wunderground::API(22030);
+    my $wun = new WWW::Wunderground::API('22030');
     my $ffx_temp = $wun->data->temp_f;
+
     $wun->location('KJFK');
     my $ny_temp = $wun->data->temp_f;
+
     $wun->location('San Diego, CA');
     my $socal_temp = $wun->data->temp_f;
 
 =head2 xml()
 
-Returns raw xml result from wunderground server
+Returns raw xml result from wunderground server where applicable
+
+=head2 json()
+
+Returns raw json result from wunderground server where applicable
 
 =head2 data()
 
 Contains xml result from server parsed into convenient Hash::AsObject form;
+
+=head2 api_key()
+
+Required for JSON api access.
+
+=head2 api_type()
+
+Defaults to xml. If an api_key is specified, this will be set to json.
 
 =head1 AUTHOR
 
@@ -147,4 +191,5 @@ See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
+__PACKAGE__->meta->make_immutable;
 1; # End of WWW::Wunderground::API
--- t/01-getweather.t
+++ t/01-getweather.t
@@ -1,6 +1,6 @@
 #!perl
 
-use Test::More tests => 5;
+use Test::More tests => 10;
 
 use_ok( 'WWW::Wunderground::API' );
 
@@ -9,3 +9,14 @@ isa_ok($wun,'WWW::Wunderground::API','Got a new Wunderground API object');
 ok(length($wun->xml),'Got XML from wunderground');
 isa_ok($wun->data,'Hash::AsObject','Parsed xml');
 like($wun->data->temp_f, qr/\d+/, 'Read temperature of '.$wun->data->temp_f.'f');
+is($wun->data->temp_f, $wun->temp_f, "Data key AUTOLOADing");
+
+SKIP: {
+  my $api_key = ''; #Set to your API key for testing
+  skip "JSON tests require an API key to be specified.", 4 unless $api_key;
+  my $wun = new WWW::Wunderground::API(location=>'KDCA', api_key=>$api_key);
+  isa_ok($wun,'WWW::Wunderground::API','Got a new Wunderground API object');
+  ok(length($wun->json),'Got JSON from wunderground');
+  isa_ok($wun->data,'Hash::AsObject','Parsed json');
+  like($wun->data->temp_f, qr/\d+/, 'Read temperature of '.$wun->data->temp_f.'f');
+}
